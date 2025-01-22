c19922fb451066dd7505be5df64684fa80973844
jmagrad <johnmagradey@gmail.com>
2025-01-22 19:06:13 +0000

    Fixed problems to pass test cases for chapter3

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
+from django.http import HttpResponse
+def index(request):
+    # Use the direct path for the about page
+    response = 'Rango says hey there partner!<br><a href="/rango/about/">About</a>'
+    return HttpResponse(response)
+
+def about(request):
+    # Use the direct path for the index page
+    response = 'Rango says here is the about page.<br><a href="/rango/">Index</a>'
+    return HttpResponse(response)
