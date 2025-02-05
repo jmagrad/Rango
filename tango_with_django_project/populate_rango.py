d9dc55dc5caf02d1ecc696b8e1f2fb8a75ce8bb7
jmagrad <johnmagradey@gmail.com>
2025-02-05 14:14:37 +0000

    Finished chapter 6 some issues with unit tests

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -1,4 +1,5 @@
 import os
+import random
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tango_with_django_project.settings')
 
@@ -50,7 +51,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], views=random.randint(1, 100))
 
     # Print out the categories we have added.
     for c in Category.objects.all():
--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -6,6 +6,11 @@ class PageAdmin(admin.ModelAdmin):
     # Specify the fields to display in the admin table
     list_display = ('title', 'category', 'url')
 
-admin.site.register(Category)
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+
+admin.site.register(Category,CategoryAdmin)
 admin.site.register(Page,PageAdmin)
 # Register your models here.
