2ba5546854cff1f6ddf60ed389dcac49d9c989b6
jmagrad <johnmagradey@gmail.com>
2025-02-15 18:55:44 +0000

    Finished chapter8 passed all tests

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -21,6 +21,7 @@ from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
